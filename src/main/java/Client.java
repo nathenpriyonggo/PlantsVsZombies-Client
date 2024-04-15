cbed51978d74593426c576fc8efd980914594b55
Nathen Priyonggo <nagapri16@gmail.com>
2024-04-15 02:53:23 -0500

    base started

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,41 +6,39 @@ import java.net.Socket;
 import java.util.function.Consumer;
 
 
-
+/*
+Client Thread Class
+ */
 public class Client extends Thread{
 
 	
-	Socket socketClient;
-	
-	ObjectOutputStream out;
-	ObjectInputStream in;
-	
+	public Socket socketClient;
+	public ObjectOutputStream out;
+	public ObjectInputStream in;
 	private Consumer<Serializable> callback;
-	
-	Client(Consumer<Serializable> call){
-	
-		callback = call;
-	}
+
+	// Default Constructor
+	Client(Consumer<Serializable> call){callback = call;}
 	
 	public void run() {
 		
 		try {
-		socketClient= new Socket("127.0.0.1",5555);
-	    out = new ObjectOutputStream(socketClient.getOutputStream());
-	    in = new ObjectInputStream(socketClient.getInputStream());
-	    socketClient.setTcpNoDelay(true);
+			socketClient= new Socket("127.0.0.1",5555);
+			out = new ObjectOutputStream(socketClient.getOutputStream());
+			in = new ObjectInputStream(socketClient.getInputStream());
+			socketClient.setTcpNoDelay(true);
 		}
 		catch(Exception e) {}
 		
 		while(true) {
-			 
+
+			// Receives input message
 			try {
-			String message = in.readObject().toString();
-			callback.accept(message);
+				String message = in.readObject().toString();
+				callback.accept(message);
 			}
 			catch(Exception e) {}
 		}
-	
     }
 	
 	public void send(String data) {
@@ -52,6 +50,4 @@ public class Client extends Thread{
 			e.printStackTrace();
 		}
 	}
-
-
 }
--- src/main/java/GuiClient.java
+++ src/main/java/GuiClient.java
@@ -1,4 +1,3 @@
-
 import java.util.HashMap;
 
 import javafx.application.Application;
@@ -16,41 +15,39 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+
 public class GuiClient extends Application{
 
-	
-	TextField c1;
-	Button b1;
-	HashMap<String, Scene> sceneMap;
-	VBox clientBox;
+
 	Client clientConnection;
-	
-	ListView<String> listItems2;
-	
-	
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+		// Callback.accept ends up here
 		clientConnection = new Client(data->{
-				Platform.runLater(()->{listItems2.getItems().add(data.toString());
+			Platform.runLater(()->{
+				// FIXME
 			});
 		});
-							
 		clientConnection.start();
 
-		listItems2 = new ListView<String>();
-		
-		c1 = new TextField();
-		b1 = new Button("Send");
-		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
-		
-		sceneMap = new HashMap<String, Scene>();
 
-		sceneMap.put("client",  createClientGui());
-		
+
+
+		// ----------------------------- Element Definitions Below -----------------------------
+
+
+
+
+
+
+		// Closing Game
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent t) {
@@ -58,22 +55,136 @@ public class GuiClient extends Application{
                 System.exit(0);
             }
         });
-
-
-		primaryStage.setScene(sceneMap.get("client"));
-		primaryStage.setTitle("Client");
+		// Setup initial scene
+		primaryStage.setScene(UsernameGUI());
+		primaryStage.setTitle("PlantShip - ZombieShip");
 		primaryStage.show();
 		
 	}
-	
 
-	
-	public Scene createClientGui() {
-		
-		clientBox = new VBox(10, c1,b1,listItems2);
-		clientBox.setStyle("-fx-background-color: blue;"+"-fx-font-family: 'serif';");
-		return new Scene(clientBox, 400, 300);
-		
+
+
+
+	// ----------------------------- GUI Functions Below -----------------------------
+
+
+
+
+	// TODO -->
+	/*
+	Username Page GUI Code
+	 	~ displays title with username text field; center
+		~ prompts user to input username
+		~ button clicked to proceed, check if unique name
+		~ if false, prompt user to input other name
+		~ if true, proceed to home page
+	 */
+	public Scene UsernameGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
 	}
 
+	/*
+	TODO -->
+	Home Page GUI Code
+		~ displays username and money; top left
+		~ displays rule page button; top right
+		~ displays title and streaks; center
+		~ option to random PvP or AI
+		~ if no players available, send wait message
+	 */
+	public Scene HomeGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
+
+	/*
+	TODO -->
+	Rules Page GUI Code
+		~ displays rules; center
+		~ back button changes scene to home page; top left
+	 */
+	public Scene RulesGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
+
+	/*
+	TODO -->
+	Player's Plants Placement GUI Code
+		~ displays tableview of buttons and 5 rows of buttons; center
+		~ user clicks desired character button
+		~ user clicks desired position
+		~ if valid position, clicked buttons disabled
+		~ if invalid, send error message
+		~ after all characters placed, enable start button
+	 */
+	public Scene PlacementGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
+
+	/*
+	TODO -->
+	Battleship Gameplay GUI Code
+		~ displays opponent's tableview of buttons; top center
+		~ displays player's tableview of imageview; bottom center
+		~ user will be notified for turns
+		~ user clicks on opponent's tableview to guess position
+		~ user will be notified if opponent guessed user table correctly
+	 */
+	public Scene GameplayGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
+
+	/*
+	TODO -->
+	Win Outcome GUI Code
+		~ displays trophy and win text
+		~ home button changes scene to home page
+	 */
+	public Scene WinGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
+
+	/*
+	TODO -->
+	Lose Outcome GUI Code
+		~ displays zombie arm and lose text
+		~ home button changes scene to home page
+	 */
+	public Scene LoseGUI() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(50));
+		pane.setStyle("-fx-background-color: white");
+
+		return new Scene(pane, 800, 500);
+	}
 }
